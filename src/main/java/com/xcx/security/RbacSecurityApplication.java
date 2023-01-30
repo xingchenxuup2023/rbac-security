1be318fa2a590864e7dea41928cae81d8db5fb0b
xingchenxu <l>
2023-01-30 17:55:01 +0800

    初始化项目

---
+++ src/main/java/com/xcx/security/RbacSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.xcx.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacSecurityApplication.class, args);
+    }
+
+}
