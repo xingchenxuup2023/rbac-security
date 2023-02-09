98ea355b2714122c8201344a73ba82cef2aaf485
xingchenxu <xingchenxuup@qq.com>
2023-02-09 18:41:16 +0800

    前端accessToken过期刷新并重新请求、refreshToken过期退出登录

--- rbac-vue/src/utils/request.js
+++ rbac-vue/src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import {Message} from 'element-ui'
 import store from '@/store'
 import {getAccessToken} from '@/utils/auth'
 
@@ -15,7 +15,7 @@ service.interceptors.request.use(
   config => {
     // do something before request is sent
 
-    if (store.getters.token) {
+    if (store.getters.accessToken) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
@@ -35,7 +35,7 @@ service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
    * Please return  response => response
-  */
+   */
 
   /**
    * Determine the request status by custom code
@@ -47,22 +47,36 @@ service.interceptors.response.use(
     // if the custom code is not 000000, it is judged as an error.
     if (res.code !== "000000") {
       Message({
-        message: res.message || 'Error',
+        message: res.msg || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.code === '999997') {
+        // R_TOKEN_EXPIRES
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
+        return new Promise(() => {
+          store.dispatch('user/logout').then(() => {
             location.reload()
           })
+        })
+
+      }
+      if (res.code === '999998') {
+        // A_TOKEN_EXPIRES
+        // 发送请求刷新token
+        const refreshToken = store.getters.refreshToken;
+        return service({
+          url: '/token/refresh',
+          method: 'get',
+          params: {refreshToken}
+        }).then(async refreshResponse => {
+          // 获取到新的token
+          let tokenInfo = refreshResponse.data
+          await store.dispatch("user/refreshAccessToken", tokenInfo)
+          // 在请求头中设置新的token
+          response.config.headers['X-Token'] = tokenInfo.accessToken
+          // 重新发送请求
+          return axios(response.config)
         })
       }
       return Promise.reject(new Error(res.message || 'Error'))
